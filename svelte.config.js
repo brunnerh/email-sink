a5cf1422c98d23cfdc8596229f885c339cd5a83c
brunnerh <brunnerh@users.noreply.github.com>
2026-08-17 07:08:10 +0200

    Use package-lock.json because Vercel. Fix issue caused by what probably is a version mismatch somewhere.

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { optimizeImports } from 'carbon-preprocess-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [optimizeImports()],
+	preprocess: [vitePreprocess({ script: true }), optimizeImports()],
 	vitePlugin: {
 		inspector: true,
 	},
